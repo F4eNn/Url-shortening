509d249eecb95e72e8709dbdc449585402a8412e
Mateusz <mateusz4k@outlook.com>
2023-05-15 20:14:26 +0200

    added entire interaction and styles for nav in all devices

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Wrapper } from '../wrapper/Wrapper'
+import { Nav } from './nav/Nav'
+import Image from 'next/image'
+import classes from './Header.module.scss'
+export const Header = () => {
+	return (
+		<header className={classes['header-bgc']}>
+			<Wrapper>
+				<Nav />
+				<div className={classes['get-started-container']}>
+					<div className={classes.description}>
+						<h1>More than just shorter links</h1>
+						<p>Build your brand's recognition and get detailes insights your links are performing</p>
+						<button>Get started</button>
+					</div>
+				<div className={classes['cropped-img']}>
+					<Image
+						src='/images/illustration-working.svg'
+						alt='working illustration'
+						width={733}
+						height={482}
+					/>
+				</div>
+				</div>
+			</Wrapper>
+		</header>
+	)
+}
---
+++ src/components/header/nav/Nav.tsx
@@ -0,0 +1,92 @@
+'use client'
+import { useState, useEffect, useRef } from 'react'
+import { Wrapper } from '../../wrapper/Wrapper'
+import classes from './Nav.module.scss'
+import Image from 'next/image'
+export const Nav = () => {
+	const [isActive, setIsActive] = useState(false)
+
+	const menuRef = useRef<HTMLDivElement>(null)
+	const showNavHandle = () => {
+		setIsActive(!isActive)
+	}
+	useEffect(() => {
+		if (isActive) {
+			document.body.style.overflow = 'hidden'
+		} else {
+			document.body.style.overflow = 'unset'
+		}
+
+		const handler = (e: UIEvent) => {
+			const target = e.target as HTMLDivElement
+			if (!menuRef.current?.contains(target)) setIsActive(false)
+		}
+
+		document.addEventListener('mousedown', handler)
+		return () => {
+			document.removeEventListener('mousedown', handler)
+		}
+	})
+	const addChangeClass = isActive ? `${classes.change}` : null
+	const showMobileMenu = isActive ? null : `${classes['hide-nav-menu']}`
+
+	return (
+		<div className={classes['hero-bg']}>
+			<Wrapper>
+				<div className={classes.container}>
+					<Image
+						src='/images/logo.svg'
+						alt=''
+						width={121}
+						height={33}
+					/>
+
+					<div ref={menuRef}>
+						<button
+							onClick={showNavHandle}
+							className={`${classes['burger-btn']} ${addChangeClass}`}>
+							<div className={classes.bar1}></div>
+							<div className={classes.bar2}></div>
+							<div className={classes.bar3}></div>
+						</button>
+						<div className={`${classes['mobile-nav']} ${showMobileMenu}`}>
+							<ul>
+								<li>
+									<button>Features</button>
+								</li>
+								<li>
+									<button>Pricing</button>
+								</li>
+								<li>
+									<button>Resources</button>
+								</li>
+							</ul>
+							<div className={classes['horizontal-line']}></div>
+							<div className={classes['button-container']}>
+								<button className={classes.login}>Login</button>
+								<button className={classes['login--signup']}>Sign Up</button>
+							</div>
+						</div>
+					</div>
+					<div className={classes['desktop-nav']}>
+						<ul>
+							<li>
+								<button>Features</button>
+							</li>
+							<li>
+								<button>Pricing</button>
+							</li>
+							<li>
+								<button>Resources</button>
+							</li>
+						</ul>
+						<div className={classes['button-container']}>
+							<button className={classes.login}>Login</button>
+							<button className={classes.signup}>Sign Up</button>
+						</div>
+					</div>
+				</div>
+			</Wrapper>
+		</div>
+	)
+}
