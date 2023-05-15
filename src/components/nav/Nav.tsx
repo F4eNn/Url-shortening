1c002393080180f1385aabeef26f7ab2be7d1a61
Mateusz <mateusz4k@outlook.com>
2023-05-15 10:32:19 +0200

    started create nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,17 +1,23 @@
 import { Wrapper } from '../wrapper/Wrapper'
-import classes from './nav.module.scss'
+import classes from './Nav.module.scss'
 import Image from 'next/image'
 export const Nav = () => {
 	return (
-		<div className={classes.box}>
+		<div className={classes['hero-bg']}>
 			<Wrapper>
-				<Image
-					src='/images/logo.svg'
-					alt=''
-					width={121}
-					height={33}
-				/>
-				<button className={classes['burger-btn']}></button>
+				<div className={classes.container}>
+					<Image
+						src='/images/logo.svg'
+						alt=''
+						width={121}
+						height={33}
+					/>
+					<button className={`${classes.burger_btn}`}>
+						<div className={classes.bar1}></div>
+						<div className={classes.bar2}></div>
+						<div className={classes.bar3}></div>
+					</button>
+				</div>
 			</Wrapper>
 		</div>
 	)
